5b005a0a5bfa585095188c58dad9810f23b5072f
Anna Tasheva <anna.tasheva90@gmail.com>
2023-07-21 22:21:44 +0100

    Updated the Navbar styling The Logo is clickable and takes the user back to the home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
 import Logo from "../assets/robot-logo.png";
 
 function Header() {
     return (
-        <header>
-            <img className="logo" src={Logo} alt="logo - smiling robot" />
-            <h1>NC News</h1>
-        </header>
+        <Link
+            to={`/`}
+            onClick={() => {
+                window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+            }}
+        >
+            <header>
+                <img className="logo" src={Logo} alt="logo - smiling robot" />
+                <h1>NC News</h1>
+            </header>
+        </Link>
     );
 }
 
