74e4249e188270109d6291909c148c2103fea94d
Anna Tasheva <anna.tasheva90@gmail.com>
2023-09-28 16:11:59 +0100

    Added a HashRouter to fix a bug when refreshing a single article page on the deployed version of the app.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { UserProvider } from "./context/User.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+    <HashRouter>
         <UserProvider>
             <App />
         </UserProvider>
-    </BrowserRouter>
+    </HashRouter>
 );
